e37e198deed28646da8341797fcf05fb0908b53a
Kim Dany <sgoiboy@gmail.com>
2022-01-14 23:48:31 +0900

    DAMIN-6 Epoxy 연동 (#4)
    * epoxy gradle
    
    * EpoxyDataBindingPattern

---
+++ app/src/main/java/damin/tothemoon/timer/EpoxyDataBindingPatterns.kt
@@ -0,0 +1,6 @@
+package damin.tothemoon.timer
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "epoxy")
+object EpoxyDataBindingPatterns
