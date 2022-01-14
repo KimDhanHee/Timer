00759302df1f72377854fe2b96297849d05ba093
kimdanhee <sgoiboy@gmail.com>
2022-01-14 23:16:14 +0900

    base fragment 구현

--- damin/src/main/java/damin/tothemoon/damin/BaseFragment.kt
+++ damin/src/main/java/damin/tothemoon/damin/BaseFragment.kt
@@ -1,4 +1,40 @@
 package damin.tothemoon.damin
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+open class BaseFragment<VDB : ViewDataBinding>(
+  @LayoutRes
+  private val layoutResId: Int,
+) : Fragment(layoutResId) {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?,
+  ): View? = DataBindingUtil.inflate<VDB>(
+    inflater,
+    layoutResId,
+    container,
+    false
+  ).run {
+    lifecycleOwner = this@BaseFragment
+
+    initView()
+    bindingVM()
+    bindingViewData()
+    setEventListener()
+
+    root
+  }
+
+  protected open fun VDB.initView() {}
+  protected open fun VDB.bindingVM() {}
+  protected open fun VDB.bindingViewData() {}
+  protected open fun VDB.setEventListener() {}
 }
