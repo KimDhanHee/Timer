60542bdd673d9f64ca7fbd260199334889467ace
Kim Dany <sgoiboy@gmail.com>
2022-01-15 09:12:59 +0900

    DAMIN-3 Navigation Component 설정 (#5)
    * nav, timer list fragment add
    
    * nav, timer fragment add

---
+++ app/src/main/java/damin/tothemoon/timer/view/TimerFragment.kt
@@ -0,0 +1,10 @@
+package damin.tothemoon.timer.view
+
+import damin.tothemoon.damin.BaseFragment
+import damin.tothemoon.timer.R
+import damin.tothemoon.timer.databinding.FragmentTimerBinding
+
+class TimerFragment : BaseFragment<FragmentTimerBinding>(
+  R.layout.fragment_timer
+) {
+}
---
+++ app/src/main/java/damin/tothemoon/timer/view/TimerListFragment.kt
@@ -0,0 +1,10 @@
+package damin.tothemoon.timer.view
+
+import damin.tothemoon.damin.BaseFragment
+import damin.tothemoon.timer.R
+import damin.tothemoon.timer.databinding.FragmentTimerListBinding
+
+class TimerListFragment : BaseFragment<FragmentTimerListBinding>(
+  R.layout.fragment_timer_list
+) {
+}
