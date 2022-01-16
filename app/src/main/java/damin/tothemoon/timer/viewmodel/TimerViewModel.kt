39126c8c28e9a91f3a4204a3354026e3a8ce138d
kimdanhee <sgoiboy@gmail.com>
2022-01-16 22:13:25 +0900

    timer view model 생성 및 구현

---
+++ app/src/main/java/damin/tothemoon/timer/viewmodel/TimerViewModel.kt
@@ -0,0 +1,58 @@
+package damin.tothemoon.timer.viewmodel
+
+import android.os.CountDownTimer
+import androidx.lifecycle.ViewModel
+import damin.tothemoon.timer.extension.timeStr
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class TimerViewModel : ViewModel() {
+  private val _timerStateFlow = MutableStateFlow<TimerState>(TimerState.Idle)
+  val timerStateFlow: StateFlow<TimerState>
+    get() = _timerStateFlow
+
+  private lateinit var timer: CountDownTimer
+  private var remainedTime: Long = 0
+
+  fun start(time: Long) {
+    timer = object : CountDownTimer(time, 1000L) {
+      override fun onTick(tick: Long) {
+        remainedTime = tick
+        _timerStateFlow.value = TimerState.CountDown(remainedTime)
+      }
+
+      override fun onFinish() {
+        _timerStateFlow.value = TimerState.Finished
+      }
+    }.start()
+  }
+
+  fun restart() {
+    start(remainedTime)
+  }
+
+  fun pause() {
+    timer.cancel()
+    _timerStateFlow.value = TimerState.Paused(remainedTime)
+  }
+
+  fun cancel() {
+    timer.cancel()
+    _timerStateFlow.value = TimerState.Canceled
+  }
+}
+
+sealed class TimerState {
+  object Idle : TimerState()
+  data class CountDown(private val remainedTime: Long) : TimerState() {
+    val remainedTimeStr = remainedTime.timeStr
+  }
+
+  data class Paused(private val remainedTime: Long) : TimerState() {
+    val remainedTimeStr = remainedTime.timeStr
+  }
+
+  object Canceled : TimerState()
+
+  object Finished : TimerState()
+}
