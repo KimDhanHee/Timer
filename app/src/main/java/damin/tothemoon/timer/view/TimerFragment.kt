a5374fc1beab0f11f13a58e073098c955ec29d20
kimdanhee <sgoiboy@gmail.com>
2022-01-16 21:02:58 +0900

    전달 받은 타이머 객체 타임 표시

--- app/src/main/java/damin/tothemoon/timer/view/TimerFragment.kt
+++ app/src/main/java/damin/tothemoon/timer/view/TimerFragment.kt
@@ -1,5 +1,6 @@
 package damin.tothemoon.timer.view
 
+import androidx.navigation.fragment.navArgs
 import damin.tothemoon.damin.BaseFragment
 import damin.tothemoon.timer.R
 import damin.tothemoon.timer.databinding.FragmentTimerBinding
@@ -7,4 +8,9 @@ import damin.tothemoon.timer.databinding.FragmentTimerBinding
 class TimerFragment : BaseFragment<FragmentTimerBinding>(
   R.layout.fragment_timer
 ) {
+  private val navArgs by navArgs<TimerFragmentArgs>()
+
+  override fun FragmentTimerBinding.initView() {
+    viewTimer.text = navArgs.timer.timeStr
+  }
 }
