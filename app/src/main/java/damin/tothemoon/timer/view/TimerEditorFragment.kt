4bc049fc65518e4296ac294ab5166127adf11100
kimdanhee <sgoiboy@gmail.com>
2022-01-17 21:06:57 +0900

    타이머 에디터 화면 추가

---
+++ app/src/main/java/damin/tothemoon/timer/view/TimerEditorFragment.kt
@@ -0,0 +1,10 @@
+package damin.tothemoon.timer.view
+
+import damin.tothemoon.damin.BaseFragment
+import damin.tothemoon.timer.R
+import damin.tothemoon.timer.databinding.FragmentTimerEditorBinding
+
+class TimerEditorFragment : BaseFragment<FragmentTimerEditorBinding>(
+  R.layout.fragment_timer_editor
+) {
+}
