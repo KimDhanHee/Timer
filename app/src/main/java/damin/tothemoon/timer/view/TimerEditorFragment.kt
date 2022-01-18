9b12adb9a7fe6b48a190d7f27c82357a7c11254e
kimdanhee <sgoiboy@gmail.com>
2022-01-18 22:25:50 +0900

    editor to timer

--- app/src/main/java/damin/tothemoon/timer/view/TimerEditorFragment.kt
+++ app/src/main/java/damin/tothemoon/timer/view/TimerEditorFragment.kt
@@ -1,15 +1,34 @@
 package damin.tothemoon.timer.view
 
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import damin.tothemoon.damin.BaseFragment
 import damin.tothemoon.timer.R
 import damin.tothemoon.timer.databinding.FragmentTimerEditorBinding
+import damin.tothemoon.timer.model.DaminTimer
 
 class TimerEditorFragment : BaseFragment<FragmentTimerEditorBinding>(
   R.layout.fragment_timer_editor
 ) {
+  private val navArgs by navArgs<TimerEditorFragmentArgs>()
+
+  private val daminTimer by lazy { navArgs.timer ?: DaminTimer(0, "", 0) }
+
   override fun FragmentTimerEditorBinding.initView() {
     arrayOf(viewHourPicker, viewMinutePicker, viewSecondsPicker).forEach { numberPicker ->
       numberPicker.setFormatter { "%02d".format(it) }
+    }
+  }
+
+  override fun FragmentTimerEditorBinding.setEventListener() {
+    viewHourPicker.setOnValueChangedListener { _, _, hour -> }
+
+    viewMinutePicker.setOnValueChangedListener { _, _, hour -> }
+
+    viewSecondsPicker.setOnValueChangedListener { _, _, hour -> }
+
+    viewStartBtn.setOnClickListener {
+      findNavController().navigate(TimerEditorFragmentDirections.actionEditorToTimer(daminTimer))
     }
   }
 }
