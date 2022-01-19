6d0cce9e2f4a1835f881c8d58e1e1fad0c24985a
kimdanhee <sgoiboy@gmail.com>
2022-01-19 21:59:04 +0900

    timeStr extension -> timerInfo data class 로 timerInfo data class 속성들 추가 및 정리

--- app/src/main/java/damin/tothemoon/timer/model/TimerInfo.kt
+++ app/src/main/java/damin/tothemoon/timer/model/TimerInfo.kt
@@ -1,7 +1,6 @@
 package damin.tothemoon.timer.model
 
 import android.os.Parcelable
-import damin.tothemoon.timer.extension.timeStr
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
@@ -25,4 +24,40 @@ data class TimerInfo(
   fun resetRemainedTime() {
     remainedTime = this.time
   }
+
+  var hour: Int
+    get() = ((time / HOUR_UNIT)).toInt()
+    set(value) {
+      time = value * HOUR_UNIT + minute * MINUTE_UNIT + seconds * SECONDS_UNIT
+      updateRemainedTime(time)
+    }
+
+  var minute: Int
+    get() = (time / MINUTE_UNIT % 60).toInt()
+    set(value) {
+      time = hour * HOUR_UNIT + value * MINUTE_UNIT + seconds * SECONDS_UNIT
+      updateRemainedTime(time)
+    }
+
+  var seconds: Int
+    get() = ((time / SECONDS_UNIT) % 60).toInt()
+    set(value) {
+      time = hour * HOUR_UNIT + minute * MINUTE_UNIT + value * SECONDS_UNIT
+      updateRemainedTime(time)
+    }
+
+  companion object {
+    const val HOUR_UNIT = 60 * 60 * 1000L
+    const val MINUTE_UNIT = 60 * 1000L
+    const val SECONDS_UNIT = 1000L
+  }
 }
+
+val Long.timeStr: String
+  get() {
+    val hour = (this / TimerInfo.HOUR_UNIT).toInt()
+    val minute = ((this / TimerInfo.MINUTE_UNIT % 60)).toInt()
+    val seconds = ((this / TimerInfo.SECONDS_UNIT) % 60).toInt()
+
+    return "%02d:%02d:%02d".format(hour, minute, seconds)
+  }
