80e14ec139f2cb29b8cd9ab4d7b63c878ba2c837
kimdanhee <sgoiboy@gmail.com>
2022-01-26 12:01:31 +0900

    timer edit title fragment 생성

---
+++ app/src/main/java/damin/tothemoon/timer/view/TimerEditTitleFragment.kt
@@ -0,0 +1,10 @@
+package damin.tothemoon.timer.view
+
+import damin.tothemoon.damin.BaseFragment
+import damin.tothemoon.timer.R
+import damin.tothemoon.timer.databinding.FragmentTimerEditTitleBinding
+
+class TimerEditTitleFragment : BaseFragment<FragmentTimerEditTitleBinding>(
+  R.layout.fragment_timer_edit_title
+) {
+}
