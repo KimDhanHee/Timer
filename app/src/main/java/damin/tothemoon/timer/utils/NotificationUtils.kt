e627c5b4b86d50ceaccd444e4d5a0cc4187bc400
kimdanhee <sgoiboy@gmail.com>
2022-01-29 21:37:56 +0900

    notification utils

---
+++ app/src/main/java/damin/tothemoon/timer/utils/NotificationUtils.kt
@@ -0,0 +1,50 @@
+package damin.tothemoon.timer.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import damin.tothemoon.damin.utils.AndroidUtils
+import damin.tothemoon.timer.MainActivity
+import damin.tothemoon.timer.R
+import damin.tothemoon.timer.model.TimerInfo
+
+object NotificationUtils {
+  private const val TIMER_ALARM_CHANNEL_ID = "timer_alarm_channel"
+
+  const val TIMER_SERVICE_NOTIFICATION_ID = 10
+
+  @RequiresApi(Build.VERSION_CODES.O)
+  fun createNotificationChannel() {
+    AndroidUtils.notificationManager.createNotificationChannel(
+      NotificationChannel(
+        TIMER_ALARM_CHANNEL_ID,
+        AndroidUtils.string(R.string.app_name),
+        NotificationManager.IMPORTANCE_HIGH
+      )
+    )
+  }
+
+  fun buildNotification(context: Context, timerInfo: TimerInfo) =
+    NotificationCompat.Builder(context, TIMER_ALARM_CHANNEL_ID)
+      .setSmallIcon(R.drawable.ic_bell_off_16)
+      .setContentTitle(timerInfo.title)
+      .setFullScreenIntent(
+        PendingIntent.getActivity(
+          context,
+          0,
+          Intent(context, MainActivity::class.java),
+          PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        ),
+        true
+      )
+      .build()
+
+  fun removeNotification(timerInfo: TimerInfo) {
+    AndroidUtils.notificationManager.cancel(timerInfo.id.toInt())
+  }
+}
