f916574d50395a5047eeef2e8e3775da5311f991
Kim Dany <sgoiboy@gmail.com>
2022-02-02 15:04:41 +0900

    DAMIN-50 다국어 (en) (#51)
    * preset string res
    
    * en string

--- app/src/main/java/damin/tothemoon/timer/model/TimerDatabase.kt
+++ app/src/main/java/damin/tothemoon/timer/model/TimerDatabase.kt
@@ -6,12 +6,13 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import damin.tothemoon.damin.extensions.ioScope
 import damin.tothemoon.damin.utils.AndroidUtils
+import damin.tothemoon.timer.R
 import kotlinx.coroutines.launch
 
 @Database(entities = [TimerInfo::class], version = 1)
 abstract class TimerDatabase : RoomDatabase() {
   abstract fun timerDao(): TimerDAO
-  
+
   companion object {
     @Volatile
     private var INSTANCE: TimerDatabase? = null
@@ -31,10 +32,26 @@ abstract class TimerDatabase : RoomDatabase() {
               super.onCreate(db)
               ioScope.launch {
                 arrayOf(
-                  TimerInfo(title = "🍜라면", time = 3 * TimerInfo.MINUTE_UNIT, color = TimerColor.Green),
-                  TimerInfo(title = "🥚계란 반숙", time = 6 * TimerInfo.MINUTE_UNIT, color = TimerColor.Black),
-                  TimerInfo(title = "🥚계란 완숙", time = 12 * TimerInfo.MINUTE_UNIT, color = TimerColor.Red),
-                  TimerInfo(title = "💪플랭크", time = 1 * TimerInfo.MINUTE_UNIT, color = TimerColor.Purple)
+                  TimerInfo(
+                    title = AndroidUtils.string(R.string.preset_timer_ramen),
+                    time = 3 * TimerInfo.MINUTE_UNIT,
+                    color = TimerColor.Green
+                  ),
+                  TimerInfo(
+                    title = AndroidUtils.string(R.string.preset_timer_soft_boiled_egg),
+                    time = 6 * TimerInfo.MINUTE_UNIT,
+                    color = TimerColor.Black
+                  ),
+                  TimerInfo(
+                    title = AndroidUtils.string(R.string.preset_timer_hard_boiled_egg),
+                    time = 12 * TimerInfo.MINUTE_UNIT,
+                    color = TimerColor.Red
+                  ),
+                  TimerInfo(
+                    title = AndroidUtils.string(R.string.preset_timer_plank),
+                    time = 1 * TimerInfo.MINUTE_UNIT,
+                    color = TimerColor.Purple
+                  )
                 ).forEach {
                   timerDao.addTimerInfo(it)
                 }
