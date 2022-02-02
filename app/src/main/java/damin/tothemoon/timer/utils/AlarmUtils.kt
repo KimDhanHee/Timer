36c9639c35ad3290e4387da7aae2e6280547e80d
kimdanhee <sgoiboy@gmail.com>
2022-02-02 10:43:48 +0900

    setAlarmClock -> setExactAndAllowWhileIdle

--- app/src/main/java/damin/tothemoon/timer/utils/AlarmUtils.kt
+++ app/src/main/java/damin/tothemoon/timer/utils/AlarmUtils.kt
@@ -4,14 +4,12 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.SystemClock
 import damin.tothemoon.damin.utils.AndroidUtils
-import damin.tothemoon.timer.MainActivity
 import damin.tothemoon.timer.model.TimerInfo
 import damin.tothemoon.timer.receiver.TimerReceiver
 
 object AlarmUtils {
-  private const val REQUEST_MAIN_ACTIVITY = 100
-
   fun setAlarm(context: Context, timerInfo: TimerInfo) {
     cancelAlarm(context, timerInfo)
 
@@ -28,16 +26,9 @@ object AlarmUtils {
       PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
     )
 
-    AndroidUtils.alarmManager.setAlarmClock(
-      AlarmManager.AlarmClockInfo(
-        System.currentTimeMillis() + timerInfo.remainedTime,
-        PendingIntent.getActivity(
-          context,
-          REQUEST_MAIN_ACTIVITY,
-          Intent(context, MainActivity::class.java),
-          PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-      ),
+    AndroidUtils.alarmManager.setExactAndAllowWhileIdle(
+      AlarmManager.ELAPSED_REALTIME_WAKEUP,
+      SystemClock.elapsedRealtime() + timerInfo.remainedTime - 1000,
       pendingIntent
     )
   }
