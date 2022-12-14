310c37a410cedefe63650ac41e271f34db3758b5
Kim Dany <sgoiboy@gmail.com>
2022-12-14 20:05:07 +0900

    design system 3요소 (color, theme, typo) 생성 (#69)
    * design system 3요소 생성
    
    * ComposeActivity 생성

---
+++ app/src/main/java/damin/tothemoon/timer/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package damin.tothemoon.timer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF7A44EC)
+val Purple700 = Color(0xFF3700B3)
+
+val Teal200 = Color(0xFF03DAC5)
+val Teal700 = Color(0xFF03DAC5)
+
+val Green = Color(0xFF22CA97)
+
+val Brown = Color(0xFFEBA337)
+
+val Red = Color(0xFFEC4444)
+
+val Blue = Color(0xFF225BCA)
+
+val Lavender = Color(0xFFD33FC4)
+
+val Black20 = Color(0xFF434343)
+val Black90 = Color(0xDD2E2E2E)
+
+val Gray = Color(0xFFA7A7A7)
+
+val White10 = Color(0x1AFFFFFF)
+val White20 = Color(0x33FFFFFF)
+val White30 = Color(0x4DFFFFFF)
