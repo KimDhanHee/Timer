aabe643d5d585ded7fe11c54f928f863e1bddd56
Kim Dany <sgoiboy@gmail.com>
2022-12-19 20:58:47 +0900

    Timer List 화면 compose 전환 (#70)
    * typo 설정
    
    * timer list screen compose
    
    * TimerInfoListScreen 호출

--- app/src/main/java/damin/tothemoon/timer/ui/theme/Type.kt
+++ app/src/main/java/damin/tothemoon/timer/ui/theme/Type.kt
@@ -1,5 +1,27 @@
 package damin.tothemoon.timer.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import damin.tothemoon.timer.R
 
-val Typography = Typography()
+val Typography = Typography(
+  headlineLarge = TextStyle(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.Bold,
+  ),
+  labelMedium = TextStyle(
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Bold,
+  ),
+  labelLarge = TextStyle(
+    fontSize = 32.sp,
+    fontWeight = FontWeight.SemiBold,
+    fontFamily = FontFamily(
+      Font(R.font.rajdhani, FontWeight.SemiBold)
+    )
+  )
+)
