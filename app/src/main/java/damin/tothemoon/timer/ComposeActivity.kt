f96238d1110802aac78bd6adff36bb9c7e5e8861
Kim Dany <sgoiboy@gmail.com>
2022-12-19 20:58:47 +0900

    Timer List 화면 compose 전환 (#70)
    * typo 설정
    
    * timer list screen compose
    
    * TimerInfoListScreen 호출

--- app/src/main/java/damin/tothemoon/timer/ComposeActivity.kt
+++ app/src/main/java/damin/tothemoon/timer/ComposeActivity.kt
@@ -1,14 +1,17 @@
 package damin.tothemoon.timer
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import damin.tothemoon.timer.ui.componenets.TimerInfoListScreen
 import damin.tothemoon.timer.ui.theme.TimerTheme
 
 class ComposeActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    window.statusBarColor = Color.WHITE
     setContent {
       TimerApp()
     }
@@ -18,5 +21,6 @@ class ComposeActivity : ComponentActivity() {
 @Composable
 private fun TimerApp() {
   TimerTheme {
+    TimerInfoListScreen()
   }
 }
