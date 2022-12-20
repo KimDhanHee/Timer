33461830ad43dbbd35b5d06156763460eda4732a
Kim Dany <sgoiboy@gmail.com>
2022-12-20 20:44:01 +0900

    타이머 리스트 화면 기능 구현 (#71)

--- app/src/main/java/damin/tothemoon/timer/ui/componenets/TimerListScreen.kt
+++ app/src/main/java/damin/tothemoon/timer/ui/componenets/TimerListScreen.kt
@@ -1,5 +1,6 @@
 package damin.tothemoon.timer.ui.componenets
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,17 +15,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import damin.tothemoon.timer.R
@@ -34,57 +38,112 @@ import damin.tothemoon.timer.viewmodel.TimerListViewModel
 
 @Composable
 fun TimerInfoListScreen(
-  timerViewModel: TimerListViewModel = viewModel(),
+  timerListViewModel: TimerListViewModel = viewModel(),
   onTimerInfoClick: (TimerInfo) -> Unit = {},
 ) {
-  Column(
-    modifier = Modifier.padding(20.dp)
-  ) {
+  Column {
     Spacer(modifier = Modifier.size(4.dp))
 
-    Header()
+    var deletable by rememberSaveable { mutableStateOf(false) }
+    Header(
+      deletable,
+      onDeleteClick = {
+        deletable = true
+      },
+      onBackClick = {
+        deletable = false
+      }
+    )
 
     Spacer(modifier = Modifier.size(24.dp))
 
-    val timerInfoList by timerViewModel.timerListFlow.collectAsState()
-    TimerInfoList(timerInfoList, onTimerInfoClick)
+    val timerInfoList by timerListViewModel.timerListFlow.collectAsState()
+    TimerInfoList(
+      timerInfoList,
+      deletable,
+      onTimerInfoClick,
+      onTimerDeleteClick = { timerInfo -> timerListViewModel.deleteTimerInfo(timerInfo) }
+    )
+
+    BackHandler(enabled = deletable) {
+      deletable = false
+    }
   }
 }
 
 @Composable
 private fun Header(
+  isDeletable: Boolean = false,
+  onDeleteClick: () -> Unit = {},
+  onAddClick: () -> Unit = {},
+  onBackClick: () -> Unit = {},
+) {
+  when {
+    isDeletable -> DeletableStateHeader(onBackClick)
+    else -> IdleStateHeader(onDeleteClick, onAddClick)
+  }
+}
+
+@Composable
+private fun DeletableStateHeader(
+  onBackClick: () -> Unit,
+) {
+  Icon(
+    painter = painterResource(id = R.drawable.ic_back_48),
+    contentDescription = null,
+    modifier = Modifier
+      .padding(start = 16.dp, end = 20.dp)
+      .clickable { onBackClick() }
+  )
+}
+
+@Composable
+private fun IdleStateHeader(
   onDeleteClick: () -> Unit = {},
   onAddClick: () -> Unit = {},
 ) {
-  Row(verticalAlignment = Alignment.CenterVertically) {
+  Row(
+    modifier = Modifier.padding(start = 20.dp, end = 16.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ) {
     Text(
       text = stringResource(id = R.string.app_name),
       modifier = Modifier.weight(1f),
       style = MaterialTheme.typography.headlineLarge,
     )
 
-    IconButton(onClick = onDeleteClick) {
-      Icon(painter = painterResource(id = R.drawable.ic_minus_48), contentDescription = null)
-    }
-
+    Icon(
+      painter = painterResource(id = R.drawable.ic_minus_48),
+      contentDescription = null,
+      modifier = Modifier.clickable { onDeleteClick() }
+    )
     Spacer(modifier = Modifier.size(4.dp))
-
-    IconButton(onClick = onAddClick) {
-      Icon(painter = painterResource(id = R.drawable.ic_plus_48), contentDescription = null)
-    }
+    Icon(
+      painter = painterResource(id = R.drawable.ic_plus_48),
+      contentDescription = null,
+      modifier = Modifier.clickable { onAddClick() }
+    )
   }
 }
 
 @Composable
 private fun TimerInfoList(
   timerInfoList: List<TimerInfo>,
+  isDeletable: Boolean = false,
   onTimerInfoClick: (TimerInfo) -> Unit = {},
+  onTimerDeleteClick: (TimerInfo) -> Unit = {},
 ) {
   LazyColumn(
+    modifier = Modifier.padding(start = 20.dp, end = 20.dp),
     verticalArrangement = Arrangement.spacedBy(8.dp)
   ) {
     items(timerInfoList, key = { it.id }) { timerInfo ->
-      TimerListItem(timerInfo, onTimerClick = { onTimerInfoClick(timerInfo) })
+      TimerListItem(
+        timerInfo,
+        isDeletable = isDeletable,
+        onTimerClick = { onTimerInfoClick(timerInfo) },
+        onTimerDeleteClick = { onTimerDeleteClick(timerInfo) }
+      )
     }
   }
 }
@@ -93,7 +152,9 @@ private fun TimerInfoList(
 private fun TimerListItem(
   timerInfo: TimerInfo,
   modifier: Modifier = Modifier,
+  isDeletable: Boolean = false,
   onTimerClick: () -> Unit = {},
+  onTimerDeleteClick: () -> Unit = {},
 ) {
   Card(
     modifier = modifier
@@ -108,10 +169,23 @@ private fun TimerListItem(
       modifier = Modifier
         .padding(20.dp)
     ) {
-      Text(
-        text = timerInfo.title,
-        style = MaterialTheme.typography.labelMedium.copy(color = Color.White)
-      )
+      Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(
+          text = timerInfo.title,
+          modifier = Modifier.weight(1f),
+          overflow = TextOverflow.Ellipsis,
+          maxLines = 1,
+          style = MaterialTheme.typography.labelMedium.copy(color = Color.White)
+        )
+        if (isDeletable) {
+          Icon(
+            painter = painterResource(id = R.drawable.ic_delete_20),
+            contentDescription = null,
+            modifier = Modifier.clickable { onTimerDeleteClick() },
+            tint = Color.White
+          )
+        }
+      }
       Spacer(modifier = Modifier.size(8.dp))
       Text(
         text = timerInfo.time.timeStr,
